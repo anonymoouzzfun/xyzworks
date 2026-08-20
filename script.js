aad929f4a420667e1d7116e8adef28a990cfbffd
anonymoouzzfun <pastamaster9847@gmail.com>
2026-08-20 21:33:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("If you see this then you like to snoop around, Don't you? Well if you find any issues contact us at xyzworks@entityarchives.lol!");
+console.log("If you see this then you like to snoop around, Don't you? Well if you find any issues contact us at hello@xyzworks.lol!");
